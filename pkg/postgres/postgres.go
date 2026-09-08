7f65c50ff13e49ec176afbafe8bc8d6f84b22499
Mikhail Belous <rellmanwinner@gmail.com>
2026-09-08 17:16:40 +0300

    add interface Pool

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -5,8 +5,19 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type Pool interface {
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Close()
+
+	OpTimeout() time.Duration
+}
 
 type ConnectionPool struct {
 	*pgxpool.Pool
